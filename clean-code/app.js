ff9a3f1c5f8b8def9054dc11e9e48a51cb32dae1
Uylia Smirnova <yuliauylia@yandex.ru>
2023-04-24 22:59:37 +0400

    fix(extended-1.2): add alt tags to created button images using js

--- clean-code/app.js
+++ clean-code/app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="button-img";
+    deleteButtonImg.alt = "delete button";
     deleteButton.appendChild(deleteButtonImg);
 
 
